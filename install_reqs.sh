000ccb2993b2dda53e3b04df0c2679558e1ceb98
Joe <jth82@drexel.edu>
2017-09-19 11:21:06 +0200

    Adding keras impl. WIP

--- install_reqs.sh
+++ install_reqs.sh
@@ -3,4 +3,5 @@
 conda install -y -c mcs07 pubchempy
 conda install -y -q -c rdkit rdkit=2017.03.3
 conda install -c glemaitre imbalanced-learn
-
+conda install -c conda-forge keras
+conda install -c anaconda tensorflow-gpu
