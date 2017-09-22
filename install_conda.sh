e8aed779c55816dc66bdf1070ef7ef863cfc1621
Joe <jth82@drexel.edu>
2017-09-22 18:02:27 +0000

    Refactoring setup scripts

---
+++ install_conda.sh
@@ -0,0 +1,4 @@
+#! /bin/bash
+wget https://repo.continuum.io/archive/Anaconda3-4.4.0-Linux-x86_64.sh
+chmod +x Anaconda3-4.4.0-Linux-x86_64.sh
+./Anaconda3-4.4.0-Linux-x86_64.sh
--- install_reqs.sh
+++ install_reqs.sh
@@ -1,8 +1,6 @@
 #! /bin/bash
-wget https://repo.continuum.io/archive/Anaconda3-4.4.0-Linux-x86_64.sh
-chmod +x Anaconda3-4.4.0-Linux-x86_64.sh
-./Anaconda3-4.4.0-Linux-x86_64.sh
-conda install -y -c mcs07 pubchempy
+conda env create -n pubchem-sklearn
+source activate pubchem-sklearn
 conda install -y -q -c rdkit rdkit=2017.03.3
 conda install -c glemaitre imbalanced-learn
 conda install -c conda-forge keras
