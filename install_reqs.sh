60ec7a433c1f40836fdd22639273c70209de4b57
Joe <jth82@drexel.edu>
2017-09-22 09:31:45 +0200

    Updating files

--- install_reqs.sh
+++ install_reqs.sh
@@ -1,5 +1,7 @@
 #! /bin/bash
-
+wget https://repo.continuum.io/archive/Anaconda3-4.4.0-Linux-x86_64.sh
+chmod +x Anaconda3-4.4.0-Linux-x86_64.sh
+./Anaconda3-4.4.0-Linux-x86_64.sh
 conda install -y -c mcs07 pubchempy
 conda install -y -q -c rdkit rdkit=2017.03.3
 conda install -c glemaitre imbalanced-learn
